09882048903e6cd20998599ad03e2fc4f08bc866
Ling Minjun <lingminjun@lingminjundeMacBook-Pro.local>
2017-01-13 19:18:24 +0800

    增加分表代码

--- src/main/java/com/lingminjun/tools/Main.java
+++ src/main/java/com/lingminjun/tools/Main.java
@@ -10,10 +10,15 @@ import java.io.IOException;
 public class Main {
     private static void help() {
         System.out.println(
-                "--txt_to_excel IN_PUT_PATH OUT_PUT_PATH [SPLIT_STRING] 按照行读取文本并转换成excel)\n" +
+                "--txt_to_excel IN_PUT_PATH OUT_PUT_PATH [SPLIT_STRING] 按照行读取文本并转换成excel\n" +
                         "\tIN_PUT_PATH: 输入文本路径,不能为空\n" +
                         "\tOUT_PUT_PATH: 输出excel地址,不能为空,请自带后缀名xlsx\n" +
                         "\tSPLIT_STRING: 分割字符,不传时默认为空格分割\n" +
+                        "\n" +
+                "--split_excel IN_PUT_PATH OUT_PUT_PATH MAX_COUNT 将一个excel进行拆分\n" +
+                        "\tIN_PUT_PATH: 输入文本路径,不能为空\n" +
+                        "\tOUT_PUT_PATH: 输出excel地址,不能为空,请自带后缀名xlsx\n" +
+                        "\tMAX_COUNT: 分割后excel最大行数控制\n" +
                         "\n");
     }
 
@@ -36,13 +41,21 @@ public class Main {
 //        }
 
         String in = "/Users/lingminjun/work/work_code/utils/src/main/resources/item_list.txt";
-        String out = "/Users/lingminjun/work/work_code/utils/src/main/resources/list.xlsx";
+        String out = "/Users/lingminjun/work/work_code/utils/src/main/resources/test.xls";
+        String out1 = "/Users/lingminjun/work/work_code/utils/src/main/resources/result/list.xlsx";
         String spl= "\t";
 
         try {
-            ExcelUtils.lineTxtToExcel(in,out,spl);
+//            ExcelUtils.lineTxtToExcel(in,out,spl);
+            ExcelUtils.splitExcel(out,out1,6000);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        Other other = new Other();
+//        Map map = other.getUisa();
+//        System.out.println(map.toString());
+//        other.pares("[{\"type\":\"REDUCE\",\"name\":\"满减送\",\"rgb\":\"0xf13c34\"},{\"type\":\"DISCOUNT\",\"name\":\"满件折\",\"rgb\":\"0x6092f3\"},{\"type\":\"FLASH\",\"name\":\"限时促销\",\"rgb\":\"0xf4ae35\"},{\"type\":\"MIX_DISCOUNT\",\"name\":\"搭配折扣\",\"rgb\":\"0xe9378b\"},{\"type\":\"NYRX\",\"name\":\"N元任选\",\"rgb\":\"0x35b79b\"},{\"type\":\"GIFT\",\"name\":\"换购\",\"rgb\":\"0x9974ff\"}]");
+//        System.out.println(map.toString());
     }
 }
--- src/main/java/com/lingminjun/tools/excel/ExcelUtils.java
+++ src/main/java/com/lingminjun/tools/excel/ExcelUtils.java
@@ -1,8 +1,15 @@
 package com.lingminjun.tools.excel;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFRow;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 
@@ -87,6 +94,11 @@ public class ExcelUtils {
     }
 
     private static void saveToFile(SXSSFWorkbook wkb, String path) throws IOException {
+        File file = new File(path);
+        File parent = file.getParentFile();
+        if (!parent.exists() || !parent.isDirectory()) {
+            parent.mkdir();
+        }
         FileOutputStream output = new FileOutputStream(path);
         wkb.write(output);
         output.flush();
@@ -98,6 +110,133 @@ public class ExcelUtils {
         for (int i = 0; i < size; i++) {
             String v = values[i];
             row.createCell(i).setCellValue(v);
+        }
+    }
+
+    private static void fillRow(SXSSFRow xrow, Row row) {
+        if(row!=null&&!(row.equals(""))){
+            int columns_num = row.getLastCellNum();//获取列数
+
+            for( int columns=0;columns<columns_num;columns++){
+                Cell cell = row.getCell(columns);
+                if(cell!=null){
+                    String value = null;
+                    switch ( cell.getCellType()) {
+                        case XSSFCell.CELL_TYPE_STRING: // 字符串
+                            value = cell.getStringCellValue();
+                            break;
+                        case XSSFCell.CELL_TYPE_NUMERIC: // 数字
+                            double strCell = cell.getNumericCellValue();
+                            value = "" + strCell;
+                            break;
+                        case XSSFCell.CELL_TYPE_BLANK: // 空值
+                            value = "";
+                            break;
+                        case XSSFCell.CELL_TYPE_BOOLEAN: // 空值
+                            boolean b = cell.getBooleanCellValue();
+                            value = "" + b;
+                            break;
+                        default:
+                            value = "";
+                            System.out.println("warning!! 不支持数据类型");
+                            break;
+                    }
+                    xrow.createCell(columns).setCellValue(value);
+                }
+            }
+        }
+    }
+
+    /**
+     * 拆分excel,多sheet忽略
+     * @param in
+     * @param out
+     * @param max
+     * @throws IOException
+     */
+    public static void splitExcel(String in, String out, int max) throws IOException {
+
+        String xls_read_Address=in;//读取
+//        String xls_write_Address=out;//写入
+
+        try {
+
+//            File excel_file = new File(xls_read_Address);//读取的文件路径
+//            FileInputStream input = new FileInputStream(excel_file);  //读取的文件路径
+//            String fileName = "D:\\excel\\xlsx_test.xlsx";
+//            XSSFWorkbook xssfWorkbook = new XSSFWorkbook( fileName);
+
+            Workbook wb = null;
+            if (in.endsWith("xlsx")) {
+                wb = new XSSFWorkbook(in);
+            } else {
+                InputStream is = new FileInputStream(in);
+                wb = new HSSFWorkbook(is);
+            }
+
+            int sheet_numbers = wb.getNumberOfSheets();//获取表的总数
+
+            if (sheet_numbers <= 0) {
+                System.out.println("没有找到 excel sheet");
+                return;
+            } else if (sheet_numbers > 1) {
+                System.out.println("warning!! 多个sheet, 只能处理第一个");
+            }
+
+            Sheet sheet = wb.getSheetAt(0);
+
+            SXSSFWorkbook in_wkb = null;//new XSSFWorkbook();
+            SXSSFSheet in_sheet= null;//wkb.createSheet("未命名");
+
+            int rows_num = sheet.getLastRowNum();//获取行数
+            int r = 0;
+            int file_num = 0;//文件个数
+
+            while (r < rows_num) {
+                Row row = sheet.getRow(r);//取得某一行   对象
+
+                if (r%max == 0 && in_wkb != null) {
+                    String pathStr = out;
+                    if (file_num > 0) {
+                        String path = out.substring(0,out.lastIndexOf('.'));
+                        String ext = out.substring(out.lastIndexOf('.'));
+                        pathStr = path + file_num + ext;
+                    }
+                    file_num++;
+
+                    saveToFile(in_wkb,pathStr);
+                    System.out.println("size:"+r%max+";save:"+pathStr);
+                    in_wkb = null;
+                    in_sheet = null;
+                }
+
+                if (in_wkb == null) {
+                    in_wkb = new SXSSFWorkbook();
+                    in_sheet = in_wkb.createSheet("未命名");
+                }
+
+                //开始检查是否重新创建
+                fillRow(in_sheet.createRow(r%max),row);
+
+                r++;//下一个
+            }
+
+            //输出Excel文件
+            if (in_wkb != null) {
+                String pathStr = out;
+                if (file_num > 0) {
+                    String path = out.substring(0,out.lastIndexOf('.'));
+                    String ext = out.substring(out.lastIndexOf('.'));
+                    pathStr = path + file_num + ext;
+                }
+                saveToFile(in_wkb,pathStr);
+                System.out.println("size:"+r%max+";save:"+pathStr);
+            }
+
+//            input.close();
+        } catch (IOException ex) {
+            System.out.println("拆分报错!!!");
+            ex.printStackTrace();
         }
     }
 }
